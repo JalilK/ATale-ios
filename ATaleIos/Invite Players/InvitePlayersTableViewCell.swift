3ea5d252a44bb26885059c49b812072ebf747d81
jalil.kennedy <jalilkennedy@gmail.com>
2018-12-29 16:58:10 -0500

    Layout InvitePlayersScreen

---
+++ ATaleIos/Invite Players/InvitePlayersTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  InvitePlayersTableViewCell.swift
+//  ATaleIos
+//
+//  Created by jalil.kennedy on 12/27/18.
+//  Copyright © 2018 jalil.kennedy. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class InvitePlayersTableViewCell: UITableViewCell {
+    @IBOutlet weak var playerImageView: UIImageView!
+    @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var separatorView: UIView!
+
+    var disposeBag = DisposeBag()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupViews()
+    }
+
+    override func prepareForReuse() {
+        disposeBag = DisposeBag()
+    }
+
+    func bind(viewModel: PlayerViewModel) {
+        viewModel.playerSharedImageDriver
+            .drive(playerImageView.rx.image)
+            .disposed(by: disposeBag)
+
+        viewModel.playerNameTextDriver
+            .drive(playerNameLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+
+    private func setupViews() {
+        separatorView.backgroundColor = UIColor.lightGray
+
+        playerImageView.layer.cornerRadius = playerImageView.frame.height / 2
+        playerImageView.layer.masksToBounds = false
+        playerImageView.clipsToBounds = true
+    }
+}
---
+++ ATaleIos/Invite Players/PlayerModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerModel.swift
+//  ATaleIos
+//
+//  Created by jalil.kennedy on 12/27/18.
+//  Copyright © 2018 jalil.kennedy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct PlayerModel {
+    var id: String
+    var name: String
+    var profilePicture: UIImage
+}
