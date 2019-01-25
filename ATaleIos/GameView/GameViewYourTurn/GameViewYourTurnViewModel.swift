c84df292499f66c68ac5becef2460959ad5a5b5a
jalil.kennedy <jalilkennedy@gmail.com>
2019-01-25 14:58:09 -0500

    Setup initial layout for GameViewYourTurnViewController

---
+++ ATaleIos/GameView/GameViewYourTurn/GameViewYourTurnViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  GameViewYourTurnViewModel.swift
+//  ATaleIos
+//
+//  Created by jalil.kennedy on 1/25/19.
+//  Copyright © 2019 jalil.kennedy. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class GameViewYourTurnViewModel {
+}
